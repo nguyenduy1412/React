523b390937a2937410b970773dfa05dcd9e5452d
nguyenduy1412 <duy2172003@gmail.com>
2023-08-31 06:39:05 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,10 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import TaiXiu from './src/screen/TaiXiu';
+import Login from './src/screen/Login';
+export default function App() {
+  return (
+    <Login ></Login>
+  );
+}
+
